9db4174fbb5cd4d55a9a34d52cf49002c39bb348
noctiswhole <anthony@creates.tech>
2025-08-10 11:37:01 -0500

    reorganize files

--- src/Window.zig
+++ src/Window.zig
@@ -1,5 +1,5 @@
 const Window = @This();
-const Renderer = @import("Renderer.zig");
+const Renderer = @import("graphics/Renderer.zig");
 const sdl3 = @import("sdl3");
 const gl = @import("gl");
 const std = @import("std");
