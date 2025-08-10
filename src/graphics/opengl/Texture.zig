ec6b73c0ce049c7eb0027f6d47b6566e87453c28
noctiswhole <anthony@creates.tech>
2025-08-10 14:01:52 -0500

    ignore surface format

--- src/graphics/opengl/Texture.zig
+++ src/graphics/opengl/Texture.zig
@@ -16,42 +16,26 @@ pub fn texture_from_file(filename: [:0]const u8) !Texture {
     gl.genTextures(1, &texture);
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    var internal_format: gl.GLint = undefined;
-    var pixel_format: gl.GLuint = undefined;
+    const pixel_format: gl.GLuint = gl.RGBA;
     const texture_type: gl.GLenum = gl.UNSIGNED_BYTE;
 
-
-    if (surface.getFormat()) | format | {
-        switch (format) {
-            .packed_rgba_8_8_8_8 => {
-                internal_format = gl.RGBA;
-                pixel_format = gl.RGBA;
-            },
-            .packed_abgr_8_8_8_8 => {
-                internal_format = gl.RGBA;
-                pixel_format = gl.RGBA;
-            },
-            else => return error.UnsupportedSurfaceFormat,
-        }
-
-        if (surface.getPixels()) | pixels | {
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-            gl.texImage2D(
-                gl.TEXTURE_2D,
-                0,
-                gl.SRGB8_ALPHA8,
-                @intCast(surface.getWidth()),
-                @intCast(surface.getHeight()),
-                0,
-                pixel_format,
-                texture_type,
-                pixels.ptr,
-            );
-            gl.generateMipmap(gl.TEXTURE_2D);
-        } else {
-            return error.MissingSurfacePixels;
-        }
+    if (surface.getPixels()) | pixels | {
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texImage2D(
+            gl.TEXTURE_2D,
+            0,
+            gl.SRGB8_ALPHA8,
+            @intCast(surface.getWidth()),
+            @intCast(surface.getHeight()),
+            0,
+            pixel_format,
+            texture_type,
+            pixels.ptr,
+        );
+        gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+        return error.MissingSurfacePixels;
     }
 
     gl.bindTexture(gl.TEXTURE_2D, 0);
